8180bee1079ab431199d632ed638cd89708314fe
willexTokiniaina <willy.alexandros@hotmail.com>
2016-12-07 22:15:33 +0300

    Update ajaxGetUser.php

--- ajaxGetUser.php
+++ ajaxGetUser.php
@@ -2,9 +2,9 @@
 //get the file contents from the server
 If (isset($_REQUEST['file'])) {
 $file = basename($_REQUEST['file']);
-echo file_get_contents('./User/'.$file);
+echo file_get_contents('./'.$file);
 } Else {
-If (is_dir('./User') && $handle = opendir('./User/')) {
+If (is_dir('./') && $handle = opendir('./')) {
 While (False !== ($entry = readdir($handle))) {
 If (!is_dir($entry)) {
 echo basename($entry)."\n";
